9af62ea5fbeda3fe2a00c61af9909ead77f131ac
Patrikol4 <doublecrt@gmail.com>
2020-04-28 15:40:57 -0300

    Config file

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php 
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'client_system';
+$appname = 'Client System';
+
+$conn = new mysqli($host, $user, $pass, $dbname) or die ('Unable to connect. Internal Error');
+
+?>
